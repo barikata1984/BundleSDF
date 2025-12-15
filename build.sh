0faeba02390f66b55e7af5d74b46f2bd14bb08db
barikata1984 <franche1984@gmail.com>
2025-12-15 17:15:49 +0900

    adapt to rtx5090 (blackwell gpus)

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 ROOT=$(pwd)
 
 # Set PyTorch library path
-export LD_LIBRARY_PATH="/usr/local/lib/python3.10/dist-packages/torch/lib:$LD_LIBRARY_PATH"
-export TORCH_LIBRARIES="/usr/local/lib/python3.10/dist-packages/torch/lib"
+export LD_LIBRARY_PATH="/usr/local/lib/python3.12/dist-packages/torch/lib:$LD_LIBRARY_PATH"
+export TORCH_LIBRARIES="/usr/local/lib/python3.12/dist-packages/torch/lib"
 
 # Additional PyTorch environment variables
-export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0"
+export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;8.9;9.0;12.0"  # Added 8.9 and 12.0 for Ada Lovelace and Blackwell, respectively
 export FORCE_CUDA=1
 export TORCH_EXTENSIONS_DIR="/tmp/torch_extensions"
 
 # Ensure PyTorch can be found
-export PYTHONPATH="/usr/local/lib/python3.10/dist-packages:$PYTHONPATH"
+export PYTHONPATH="/usr/local/lib/python3.12/dist-packages:$PYTHONPATH"
 
 # Print debug info
 echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
